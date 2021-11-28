e9efc7066bdf5e94966e9d015cb1ef937c5d6f0a
Goja <gortnar@beograd.com>
2021-11-28 15:03:13 +0100

    Serviranje statičkoh resursa - 28.11.2021.

--- config/storage.config.ts
+++ config/storage.config.ts
@@ -1,6 +1,8 @@
 export const StorageConfig = {
     slika: {
         destinacija: '../storage/slike/',
+        urlPrefix: '/assets/slike',
+        maxAge: 1000 * 60 * 60 * 24 * 7,
         maxSize: 1024 * 1024 * 5,
         resize: {
             thumb: {
